3ac62027f950a113dffe0a28710481e3d712200e
462441355@qq.com <462441355@qq.com>
2023-06-27 13:58:57 +0800

    * 配置路径

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 import { Wx2Ant } from "./src/index";
 const path = require("path");
 let configpath = path.resolve(__dirname, "./src/config/index.txt");
+let dir = path.resolve(__dirname, "./wx");
 
 const options = {
-    configpath
+    configpath,
+    dir
 }
 
 new Wx2Ant(options)
